294f075ca9c18112224ef587e3eeb5481a37e0c0
yasir19 <yasir.habboo@gmail.com>
2022-02-01 13:21:05 -0600

    adding comment attribute and routes

--- router/api/index.js
+++ router/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const userRouters = require('./user-routes');
 const postRouters = require('./post-routes');
+const commentRouters = require('./comment_routes');
+
 
 router.use('/users', userRouters);
 router.use('/posts', postRouters);
-
+router.use('/comment',commentRouters);
 
 module.exports = router;
